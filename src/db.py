228e908b7a56b7b966ec5444767a8245f55fc18a
Rohit Kaushik <kaushikrohit325@gmail.com>
2019-12-12 10:25:37 +0100

    add database handler and util to convert csv data to sqlite db

--- src/db.py
+++ src/db.py
@@ -1,1 +1,82 @@
+import sqlite3
+from sqlite3 import Error
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import remove_stopwords
+
+create_table_query = ("create table if not exists articles "
+                     "(source TEXT, "
+                     "day INTEGER, "
+                     "month INTEGER, "
+                     "year INTEGER, "
+                     "program_name TEXT, "
+                     "transcript TEXT, "
+                     "PRIMARY KEY (source, day, month, year, program_name));")
+
+insert_table_query = ("insert or replace into articles(source, day, month, year, program_name, "
+                      "transcript) values(?, ?, ?, ?, ?, ?);")
+
+select_articles_by_year_query = "select * from articles where year=?"
+
+select_articles_by_month_query = "select * from articles where month=?"
+
+select_articles_by_date_query = "select * from articles where day=? and month=? and year=?"
+
+
+class articles_database():
+
+    def __init__(self, path):
+        self.conn = None
+        self.path = path
+
+        try:
+            self.conn = sqlite3.connect(path)
+        except Error as e:
+            print(e)
+
+        self._create_table()
+
+    def insert_articles(self, articles):
+        try:
+            cur = self.conn.cursor()
+            cur.executemany(insert_table_query, articles)
+            self.conn.commit()
+        except Error as e:
+            print(e)
+
+    def select_articles_by_year(self, year):
+        cur = self.conn.cursor()
+        rows = cur.execute(select_articles_by_year_query, (year,))
+
+        return ResultIterator(rows)
+
+    def select_articles_by_month(self, month):
+        cur = self.conn.cursor()
+        rows = cur.execute(select_articles_by_month_query, (month,))
+
+        return ResultIterator(rows)
+
+    def select_articles_by_date(self, date):
+        cur = self.conn.cursor()
+        cur.execute(select_articles_by_date_query, (date.day, date.month, date.year))
+        rows = cur.fetchall()
+
+        return ResultIterator(rows)
+
+    def _create_table(self):
+        try:
+            cur = self.conn.cursor()
+            cur.execute(create_table_query)
+            self.conn.commit()
+        except Error as e:
+            print(e)
+
+
+class ResultIterator():
+
+    def __init__(self, rows):
+        self.rows = rows
+
+    def __iter__(self):
+        for row in self.rows:
+            yield simple_preprocess(remove_stopwords(row[5]))
 
--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,14 @@ from dateutil import parser as date_parser
 import pandas as pd
 import os
 import pickle
+import db
+
+months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 
 def save(obj, name):
     with open(name, 'wb') as f:
         pickle.dump(obj, f)
-        
+
 def load_data(path):
     newspapers = os.listdir(path)
     df_daywise = []
@@ -17,15 +20,57 @@ def load_data(path):
         df = pd.read_csv(df_path)
         df = df.drop(df.columns[0], axis=1)
         df = df.dropna()
-        
+
         #rename columns so it is consistent across data
         df.columns = ['Source', 'Date', 'Program Name', 'Transcript']
         df = df[(df['Program Name'] != "") & (df['Transcript'] != '')]
-        
+
         #parse date
         df['Date'] = df.apply(lambda row: row['Date'].split('\n')[0], axis=1)
         df['Date'] = df.apply(lambda row: date_parser.parse(row['Date']), axis=1)
-        
+
         df_daywise.append(df)
 
-    return pd.concat(df_daywise, axis=0, ignore_index=True).reset_index()
+    df_out = pd.concat(df_daywise, axis=0, ignore_index=True).reset_index()
+    df_out.to_pickle('./Jan_2014.pkl')
+    return df_out
+
+def parse_date(date):
+    date = date.split('\n')[0]
+
+    if '/' in date:
+        date = date.split('/')[1]
+    elif '-' in date:
+        date = date.split('-')[1]
+
+    return date_parser.parse(date)
+
+def raw_data_to_db(root):
+    newspapers_id = os.listdir(root)
+    conn = db.articles_database('./articles.db')
+
+    for newspaper in newspapers_id:
+        newspaper_path = os.path.join(root, newspaper)
+        years = os.listdir(newspaper_path)
+        years = [year for year in years if not year.endswith('.pkl')]
+
+        for year in years:
+            year_path = os.path.join(newspaper_path, year)
+
+            for month in months:
+                month_path = os.path.join(year_path, ' {}-{} {}.csv'.format(month, year, newspaper))
+                print("newspaper: {}, year: {}, month: {}".format(newspaper, year, month))
+                if os.path.exists(month_path):
+                    df = pd.read_csv(month_path)
+                    df = df.drop(df.columns[0], axis=1)
+                    df = df.dropna()
+                    df.columns = ['Source', 'Date', 'Program Name', 'Transcript']
+                    df = df[(df['Program Name'] != "") | (df['Transcript'] != "")]
+
+                    if df.empty:
+                        continue
+
+                    df['Date'] = df.apply(lambda row: parse_date(row['Date']), axis=1)
+                    articles = df.apply(lambda row: (row['Source'], row['Date'].day, row['Date'].month, row['Date'].year, row['Program Name'], row['Transcript']), axis=1).tolist()
+
+                    conn.insert_articles(articles)
