69c46b861553e9a238768ce5978d4654cda6da17
Rohit Kaushik <kaushikrohit325@gmail.com>
2019-12-14 23:10:52 +0100

    update database

--- src/db.py
+++ src/db.py
@@ -1,7 +1,8 @@
 import sqlite3
 from sqlite3 import Error
-from gensim.utils import simple_preprocess
-from gensim.parsing.preprocessing import remove_stopwords
+from models import Article
+from datetime import date
+from copy import copy
 
 create_table_query = ("create table if not exists articles "
                      "(source TEXT, "
@@ -12,14 +13,22 @@ create_table_query = ("create table if not exists articles "
                      "transcript TEXT, "
                      "PRIMARY KEY (source, day, month, year, program_name));")
 
-insert_table_query = ("insert or replace into articles(source, day, month, year, program_name, "
+insert_table_query = ("insert on conflict ignore into articles(source, day, month, year, program_name, "
                       "transcript) values(?, ?, ?, ?, ?, ?);")
 
 select_articles_by_year_query = "select * from articles where year=?"
 
-select_articles_by_month_query = "select * from articles where month=?"
+select_articles_by_month_query = "select * from articles where year=? and month=?"
 
 select_articles_by_date_query = "select * from articles where day=? and month=? and year=?"
+
+get_count_by_year_query = "select count(*) from articles where year=?"
+
+get_count_by_month_and_year_query = "select count(*) from articles where year=? and month=?"
+
+get_count_by_date_query = "select count(*) from articles where day=? and month=? and year=?"
+
+get_all_distinct_source_query = "select distinct(source) from articles;"
 
 
 class articles_database():
@@ -27,12 +36,7 @@ class articles_database():
     def __init__(self, path):
         self.conn = None
         self.path = path
-
-        try:
-            self.conn = sqlite3.connect(path)
-        except Error as e:
-            print(e)
-
+        self._get_connection(path)
         self._create_table()
 
     def insert_articles(self, articles):
@@ -49,18 +53,53 @@ class articles_database():
 
         return ResultIterator(rows)
 
-    def select_articles_by_month(self, month):
+    def select_articles_by_year_and_month(self, year, month):
         cur = self.conn.cursor()
-        rows = cur.execute(select_articles_by_month_query, (month,))
+        rows = cur.execute(select_articles_by_month_query, (year, month,))
 
         return ResultIterator(rows)
 
     def select_articles_by_date(self, date):
         cur = self.conn.cursor()
-        cur.execute(select_articles_by_date_query, (date.day, date.month, date.year))
-        rows = cur.fetchall()
+        rows = cur.execute(select_articles_by_date_query, (date.day, date.month, date.year))
 
         return ResultIterator(rows)
+
+    def get_count_of_articles_for_year_and_month(self, year, month):
+        try:
+            cur = self.conn.cursor()
+            cur.execute(get_count_by_month_and_year_query, (year, month),)
+        except Error as e:
+            print(e)
+
+        return cur.fetchone()[0]
+
+    def get_count_of_articles_for_year(self, year):
+        cur = self.conn.cursor()
+        cur.execute(get_count_by_year_query, (year,))
+
+        return cur.fetchone()[0]
+
+    def get_count_of_articles_for_date(self, date):
+        cur = self.conn.cursor()
+        cur.execute(get_count_by_date_query, (date.day, date.month, date.year))
+
+        return cur.fetchone()[0]
+
+    def get_all_new_source(self):
+        cur = self.conn.cursor()
+        cur.execute()
+
+        return cur.fetchall()
+
+    def _get_connection(self, path):
+        if self.conn != None:
+            return
+
+        try:
+            self.conn = sqlite3.connect(path)
+        except Error as e:
+            print(e)
 
     def _create_table(self):
         try:
@@ -70,6 +109,9 @@ class articles_database():
         except Error as e:
             print(e)
 
+    def close(self):
+        self.conn.close()
+        self.conn = None
 
 class ResultIterator():
 
@@ -78,5 +120,5 @@ class ResultIterator():
 
     def __iter__(self):
         for row in self.rows:
-            yield simple_preprocess(remove_stopwords(row[5]))
+            yield Article(row[0], date(row[3], row[2], row[1]), row[4], row[5])
 
