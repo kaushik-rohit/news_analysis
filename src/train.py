dbd521a1f794f6127025da7fc2b2090b74a96b84
Rohit Kaushik <kaushikrohit325@gmail.com>
2019-12-29 22:25:44 +0100

    train: don't serialize corpus
    + fix some comments and log statements

--- src/train.py
+++ src/train.py
@@ -27,32 +27,42 @@ parser.add_argument('-y', '--year',
                     default=2014)
 
 
-def train_tfidf(docs, n, out, name):
-    assert(n == len(docs))
+def train_tfidf(docs, out, name):
+    """
+    Parameters
+    ----------
+    @docs: A list of strings, corpus on which TfIDF model will be trained
+    @out: directory path where bag of words and tfidf model will be saved
+    @name: suffix to be added to the models name, i.e if 2014 is passed as
+        name arg, the saved models will be named tfidf_2014 and vocab_2014.dict
+
+    Returns
+    -------
+    None
+    """
     # build bag of words
     print('Building vocabulary for the corpus')
     corpus = CorpusIter(docs)
     dictionary = corpora.Dictionary(iter(corpus))
 
-    print('length of dict before filtering is {}'.format(len(dictionary)))
     print('Bag of Words Done!! Trying feature reduction')
+    print('length of dict before filtering is {}'.format(len(dictionary)))
     # downscale/feature reduction for dictionary
     # with a large dataset dictionary size can grow huge
     # leading to large tfidf and hence high runtime for transform to bow
     # and calculating similarities
-    dictionary.filter_extremes(no_above=0.95)
+    dictionary.filter_extremes(no_above=0.90)
 
     print('length of dict after filtering is {} ...'.format(len(dictionary)))
     print('creating bag of words for corpus and building tfidf model')
     # create tfidf
     bow_corpus = BoWIter(dictionary, docs)
-
     tfidf = models.TfidfModel(iter(bow_corpus))
 
     print('Saving trained models')
     # save the models for later use
     dictionary.save(os.path.join(out, 'vocab_{}.dict'.format(name)))
-    corpora.MmCorpus.serialize(os.path.join(out, 'bow_corpus_{}.mm'.format(name)), iter(BoWIter(dictionary, docs)))
+    # corpora.MmCorpus.serialize(os.path.join(out, 'bow_corpus_{}.mm'.format(name)), iter(BoWIter(dictionary, docs)))
     tfidf.save(os.path.join(out, 'tfidf_{}'.format(name)))
 
 
@@ -62,12 +72,14 @@ def main():
     conn = db.ArticlesDb(args.db_path)
     if args.month is None:
         n = conn.get_count_of_articles_for_year(args.year)
-        corpus = conn.select_articles_by_year(args.year)
-        train_tfidf(list(corpus), n, args.output_path, '{}'.format(args.year))
+        corpus = list(conn.select_articles_by_year(args.year))
+        assert(n == len(corpus))
+        train_tfidf(corpus, args.output_path, '{}'.format(args.year))
     else:
         n = conn.get_count_of_articles_for_year_and_month(args.year, args.month)
-        corpus = conn.select_articles_by_year_and_month(args.year, args.month)
-        train_tfidf(list(corpus), n, args.output_path, '{}_{}'.format(args.year, args.month))
+        corpus = list(conn.select_articles_by_year_and_month(args.year, args.month))
+        assert(n == len(corpus))
+        train_tfidf(corpus, args.output_path, '{}_{}'.format(args.year, args.month))
 
 
 if __name__ == '__main__':
