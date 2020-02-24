6b2ad9eef894aa70f69960cd185b87788493e530
Rohit Kaushik <kaushikrohit325@gmail.com>
2020-02-25 00:33:26 +0100

    tests: add test for cluster_analysis/get_pos_of_same_source_news

---
+++ tests/test_cluster_analysis.py
@@ -0,0 +1,26 @@
+import unittest
+import src.cluster_analysis as cluster_analysis
+
+
+class FakeArticle:
+
+    def __init__(self, source):
+        self.source = source
+
+
+class TestClusterAnalysis(unittest.TestCase):
+
+    def test_get_pos_of_same_source_news(self):
+        corpus = [FakeArticle('Guardian'), FakeArticle('Independent'), FakeArticle('Guardian'), FakeArticle('Mirror')]
+        res = cluster_analysis.get_pos_of_same_source_news(corpus)
+        print(res)
+
+    def test_get_pos_of_same_source_news2(self):
+        corpus1 = [FakeArticle('Guardian'), FakeArticle('Independent'), FakeArticle('Guardian'), FakeArticle('Mirror')]
+        corpus2 = [FakeArticle('Independent'), FakeArticle('Mirror'), FakeArticle('Mirror'), FakeArticle('Guardian')]
+        res = cluster_analysis.get_pos_of_same_source_news(corpus1, corpus2)
+        print(res)
+
+
+if __name__ == '__main__':
+    unittest.main()
