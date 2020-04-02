cf0825481ac47a53a3300b7edcebd22dbe25db55
Rohit Kaushik <kaushikrohit325@gmail.com>
2020-04-02 02:21:51 +0200

    topics: add topics classification using lda

--- src/topics.py
+++ src/topics.py
@@ -1,9 +1,12 @@
 import argparse
 import db
-from models import BoWIter1
+from datetime import date, timedelta
+import calendar
+from models import CorpusIter, BoWIter
 from gensim import corpora, models
 from gensim.corpora.mmcorpus import MmCorpus
 from gensim.similarities import MatrixSimilarity
+from gensim.models.phrases import Phrases, Phraser
 import bigrams
 import helpers
 import numpy as np
@@ -29,6 +32,12 @@ parser.add_argument('-y', '--year',
                     type=int,
                     default=2014,
                     help='The year for which analysis is to be performed. By default takes the value 2014')
+
+parser.add_argument('-algo', '--algorithm',
+                    type=str,
+                    choices=['lda', 'tfidf', 'word2vec'],
+                    default='tfidf',
+                    help='which training algorithm to use')
 
 parser.add_argument('-t', '--threshold',
                     type=float,
@@ -86,20 +95,60 @@ def build_news_articles_corpus(articles, out, name):
     tfidf.save(os.path.join(out, 'topics_tfidf_{}'.format(name)))
 
 
-def assign_topics_to_articles_for_month(db_path, year, month):
+def get_bigram_model_lda(articles):
+    preprocess_fn = helpers.preprocess_text_for_lda
+    corpus = CorpusIter(articles, preprocess_fn)
+    phrases = Phrases(corpus, min_count=1, threshold=1)
+    bigram = Phraser(phrases)
+
+    return bigram
+
+
+def assign_topics_to_articles_for_month_lda(db_path, year, month):
     conn = db.NewsDb(db_path)
 
+    print('getting articles')
+    preprocess_fn = helpers.preprocess_text_for_lda
     articles = list(conn.select_articles_by_year_and_month(year, month))
-    topics = []
+    corpus = CorpusIter(articles, preprocess_fn)
 
-    print('loading models')
+    print('get bigram model')
+    bigram = get_bigram_model_lda(articles)
+    bigram_corpus = bigram[iter(corpus)]
+
+    print('building dictionary')
+    dictionary = corpora.Dictionary(bigram_corpus)
+    dictionary.filter_extremes(no_below=10, no_above=0.4)
+    print('size of vocab: {}'.format(len(dictionary)))
+
+    bow_articles = list(iter(BoWIter(dictionary, articles, preprocess_fn, bigram)))
+
+    print('training lda')
+    lda_model = models.ldamulticore.LdaMulticore(corpus=bow_articles,
+                                                 id2word=dictionary,
+                                                 num_topics=20)
+
+    print(lda_model.print_topics())
+
+    print(lda_model[bow_articles[0]])
+    lda_model.save('../models/lda_{}_{}'.format(year, month))
+    conn.close()
+
+
+def assign_topics_to_articles_for_date_tfidf(db_path, cur_date):
+    conn = db.NewsDb(db_path)
+    articles = list(conn.select_articles_by_date(cur_date))
+
+    print('assign topics to articles for {}'.format(cur_date))
+
     topics_corpus = MmCorpus('../models/topics_corpus.mm')
     dct = corpora.Dictionary.load('../models/topics_vocab.dict')
-    tfidf = models.TfidfModel.load('../models/topics_tfidf_2015_1')
 
     bigram_for_articles = bigrams.transform_corpus_to_bigrams(articles)
-    bow_articles = BoWIter1(dct, bigram_for_articles)
+    bow_articles = BoWIter(dct, bigram_for_articles)
+    tfidf = models.TfidfModel(iter(bow_articles))
     tfidf_articles = tfidf[bow_articles]
+    topics = []
 
     index = MatrixSimilarity(topics_corpus, num_features=len(dct))
     for idx, similarities in enumerate(index[tfidf_articles]):
@@ -110,10 +159,19 @@ def assign_topics_to_articles_for_month(db_path, year, month):
         else:
             topics.append(helpers.topics_id_to_name_map[helpers.topics_id[max_sim_index]])
 
-    print('updating topics for all articles into db')
     conn.update_topic_for_articles(articles, topics)
 
     conn.close()
+
+
+def assign_topics_to_articles_for_month_tfidf(db_path, year, month):
+    start_date = date(year, month, 1)
+    end_date = date(year, month, calendar.monthrange(year, month)[1])  # calendar.monthrange(year, month)[1]
+
+    date_range = [start_date + timedelta(days=x) for x in range((end_date - start_date).days + 1)]
+    print('assign topics to articles for {} {}'.format(year, calendar.month_name[month]))
+    for curr_date in date_range:
+        assign_topics_to_articles_for_date_tfidf(db_path, curr_date)
 
 
 def main():
@@ -126,7 +184,8 @@ def main():
     # build_news_articles_corpus(articles, '../models', '2015_1')
     # conn.close()
     # build_dictionary_and_corpus(db_path, '../models')
-    assign_topics_to_articles_for_month(db_path, year, month)
+    # assign_topics_to_articles_for_month_tfidf(db_path, year, month)
+    assign_topics_to_articles_for_month_lda(db_path, year, month)
 
 
 if __name__ == '__main__':
